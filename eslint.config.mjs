cd5f8cc3d15ddbb24f3794e62891cc516fdf9c05
MaivandRahmani <103935508+Kingnew2006@users.noreply.github.com>
2026-09-06 00:33:27 +0300

    chore(refactor): P1 fixup - ignore nested apps/web/.next in root eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
-const config = [...nextVitals, ...nextTypescript];
+const config = [
+  // monorepo: eslint-config-next's ".next/**" ignore is config-dir-relative,
+  // so also ignore the app's nested build dir explicitly
+  { ignores: ["**/.next/**"] },
+  ...nextVitals,
+  ...nextTypescript,
+];
 
 export default config;
